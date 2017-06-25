a9693c03cd0eddfac3ea7737adb57838c0696553
Anton Zaripov <ntnzaripov@gmail.com>
2017-06-25 12:11:14 +0300

    Update answers from bot

--- src/main/java/UmerBot.java
+++ src/main/java/UmerBot.java
@@ -37,6 +37,8 @@ public class UmerBot extends TelegramLongPollingBot {
         }
     }
 
+
+
     @Override
     public String getBotUsername() {
         return resourceBundle.getProperty("alias");
