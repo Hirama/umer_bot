a3ecad321e168a12cbfc0c8a92e80e82a5b1166a
Anton Zaripov <ntnzaripov@gmail.com>
2017-06-25 11:51:19 +0300

    Start work

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.generics.BotSession;
 
 /**
  * Created by andrej on 25.06.17.
@@ -7,9 +9,16 @@ import org.telegram.telegrambots.exceptions.TelegramApiException;
 public class Main {
     public static void main(String[] args) {
 
+
+        ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
-            telegramBotsApi.registerBot(new UmerBot());
+            BotSession session = telegramBotsApi.registerBot(new UmerBot());
+            Runtime.getRuntime().addShutdownHook(new Thread(
+                    ()->{
+                        session.stop();
+                        System.out.println("Феерический уход гуся.");
+                    }));
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
--- src/main/java/UmerBot.java
+++ src/main/java/UmerBot.java
@@ -1,39 +1,55 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
-import org.telegram.telegrambots.generics.BotOptions;
-import org.telegram.telegrambots.generics.LongPollingBot;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
 
 /**
  * Created by andrej on 25.06.17.
  */
-public class UmerBot implements LongPollingBot {
+public class UmerBot extends TelegramLongPollingBot {
+
+    Properties resourceBundle = new Properties();
+
+    {
+        try {
+            resourceBundle.load(new InputStreamReader(ClassLoader.getSystemResourceAsStream("tg.properties")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     @Override
     public void onUpdateReceived(Update update) {
-
+        if (update.getMessage().getText().length() > 2) {
+            SendMessage message = new SendMessage();
+            message.setText("Умер");
+            message.setChatId(update.getMessage().getChatId());
+            try {
+                sendMessage(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return resourceBundle.getProperty("alias");
     }
 
     @Override
     public String getBotToken() {
-        return null;
-    }
-
-    @Override
-    public BotOptions getOptions() {
-        return null;
-    }
-
-    @Override
-    public void clearWebhook() throws TelegramApiRequestException {
-
+        return resourceBundle.getProperty("token");
     }
 
     @Override
     public void onClosing() {
 
     }
+
 }
